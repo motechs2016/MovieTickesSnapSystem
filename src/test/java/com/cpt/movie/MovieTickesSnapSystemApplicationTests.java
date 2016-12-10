1ae75cc492f51db66a4a2f5a183a7573cf7aaad5
cpt725 <cpt725@qq.com>
2016-12-10 10:43:22 +0800

    创建工程

---
+++ src/test/java/com/cpt/movie/MovieTickesSnapSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cpt.movie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieTickesSnapSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
